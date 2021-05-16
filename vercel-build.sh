4d73bf12917ec88b06219f721a69a7b93ac1bdcb
Marshall Bowers <elliott.codes@gmail.com>
2021-05-16 14:04:40 -0400

    Deploy into a subdirectory

--- vercel-build.sh
+++ vercel-build.sh
@@ -6,4 +6,4 @@ yum install wget
 
 wget -q -O - "https://github.com/rust-lang/mdBook/releases/download/v$MDBOOK_VERSION/mdbook-v$MDBOOK_VERSION-x86_64-unknown-linux-gnu.tar.gz" | tar xzf - -C /usr/local/bin
 
-mdbook build
+mdbook build --dest-dir ./book/knowledge
